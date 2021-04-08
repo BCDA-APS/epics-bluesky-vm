eb5cf1ff405fc389976f16dc50d693a14a5d8638
Pete R Jemian <prjemian@gmail.com>
2021-04-08 16:45:24 -0500

    MNT #16

--- .create/09b_create_conda_environment.sh
+++ .create/09b_create_conda_environment.sh
@@ -11,6 +11,7 @@ sed -i s:'- super-state-machine':'# - super-state-machine':g ~/Downloads/environ
 sed -i s:'- sphinx-rtd-theme':'# - sphinx-rtd-theme':g ~/Downloads/environment_2021_1.yml
 sed -i s:'- happi':'# - happi':g ~/Downloads/environment_2021_1.yml
 
+source ${CONDA_PREFIX}/bin/activate
 conda env create -f ~/Downloads/environment_2021_1.yml
 conda env list
 
