8371578858c7cea0e3d182956cc2e3abfdd93ffd
Pete R Jemian <prjemian@gmail.com>
2021-04-08 15:33:11 -0500

    MNT #16

--- .create/09d_configure_ipython.sh
+++ .create/09d_configure_ipython.sh
@@ -14,6 +14,8 @@ cat >> ~/.bash_aliases << EOF
 export IPYTHON_DIR=${IPYTHON_DIR}
 EOF
 
+source ${CONDA_PREFIX}/bin/activate
 conda activate ${BLUESKY_ENVIRONMENT}
+
 ipython profile create --ipython-dir=${IPYTHON_DIR} --profile=bluesky
 cp ~/bluesky/run_instrument.py ${IPYTHON_DIR}/profile_bluesky/startup/
