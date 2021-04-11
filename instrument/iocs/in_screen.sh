42d0a9e325013921fca69e8e8ba2dee48b7078d5
Pete R Jemian <prjemian@users.noreply.github.com>
2021-04-11 09:18:16 -0500

    MNT #16 path

--- instrument/iocs/in_screen.sh
+++ instrument/iocs/in_screen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # file: in_screen.sh
-export BASE=$(dirname bash)
-/usr/bin/screen -d -m -S IOC_registers -h 5000 /run_ioc.sh
+
+SHELL_SCRIPT_NAME=${BASH_SOURCE:-${0}}
+export THISDIR=$(dirname $(readlink -f ${SHELL_SCRIPT_NAME}))
+/usr/bin/screen -d -m -S IOC_registers -h 5000 ${THISDIR}/run_ioc.sh
 echo $(screen -ls)
